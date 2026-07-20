4c44ef8b5c36c0d4bb6264b6415f2913dbf3d136
jossiewang <jossiew621@gapp.nthu.edu.tw>
2026-07-20 17:36:37 +0200

    Added: BO for RMSE

--- vertical/s06_data.m
+++ vertical/s06_data.m
@@ -13,7 +13,7 @@ addpath('../mbc');
 addpath('../mpc');
 
 %% Clear Workspace
-clear all;
+% clear all;
 
 %% Solver Settings
 P_dt = 25e-3; % Sampling time of controller and planner
@@ -31,7 +31,7 @@ d71_data;
 t71_mad76_small;
 
 %% mpc params
-mpc_params_script;
+% mpc_params_script;
 
 %% Location
 P_c_locate_problist = ones(P_car_cnt, 1, 'single'); % observer all cars
