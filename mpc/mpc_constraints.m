0e046427e804b8bd62df1917d96919d07a27666e
jossiewang <jossiew621@gapp.nthu.edu.tw>
2026-07-20 18:32:36 +0200

    Modified: significant runtime improvement from matrix preallocation ----------- note: instead of c = [c; ...]; in a loop, preallocate the whole size of c = zeros(8*N, 1);

--- mpc/mpc_constraints.m
+++ mpc/mpc_constraints.m
@@ -1,69 +1,58 @@
 function [c, ceq] = mpc_constraints(U, z0, u_last, refs, params)
-%UNTITLED6 Summary of this function goes here
-%   Detailed explanation goes here
 
     U      = double(U(:));
-    z0     = double(z0(:));
-    u_last = double(u_last(:));
-    
-    N = params.N;
-    
-    % initialization
-    z = z0(:);
-    u_prev = u_last(:);
-    
-    c   = zeros(0,1);
-    ceq = zeros(0,1);
+    z      = double(z0(:));
+    u_prev = double(u_last(:));
 
-    % get params
+    N = params.N;
+
     v_min = params.v_min;
     v_max = params.v_max;
     track_half_width = params.track_half_width;
     dun_max = params.dun_max;
     ddeltan_max = params.ddeltan_max;
 
+    c = zeros(8*N, 1);
+    ceq = [];
+
+    k = 1;
+
     for i = 1:N
         idx = 2*i - 1;
-        u_i = U(idx:idx+1);
+        un_i     = U(idx);
+        deltan_i = U(idx+1);
 
-        %% Rate constraints
-        du = u_i - u_prev;
+        du1 = un_i     - u_prev(1);
+        du2 = deltan_i - u_prev(2);
 
-        c = [c;
-            du(1)  - dun_max;
-            -du(1)  - dun_max;
-            du(2)  - ddeltan_max;
-            -du(2)  - ddeltan_max];
+        % Rate constraints
+        c(k) =  du1 - dun_max;       k = k + 1;
+        c(k) = -du1 - dun_max;       k = k + 1;
+        c(k) =  du2 - ddeltan_max;   k = k + 1;
+        c(k) = -du2 - ddeltan_max;   k = k + 1;
 
-        %% Predict next state
+        % Predict next state
+        u_i = [un_i; deltan_i];
         z = mpc_model_step(z, u_i, params);
 
-        %% Speed constraints
+        % Speed constraints
         vr = z(1);
+        c(k) = vr - v_max;           k = k + 1;
+        c(k) = v_min - vr;           k = k + 1;
 
-        c = [c;
-            vr - v_max;
-            v_min - vr];
+        % Lateral corridor constraint
+        ds1 = z(2) - refs.s(1, i+1);
+        ds2 = z(3) - refs.s(2, i+1);
 
-        %% Track/lateral corridor constraint
-        s_pred = [z(2); z(3)];
+        n1 = refs.n(1, i+1);
+        n2 = refs.n(2, i+1);
 
-        % Use reference index i+1 because z is already propagated
-        s_ref = refs.s(:, i+1);
-        n_ref = refs.n(:, i+1);
+        s_c2e = ds1*n1 + ds2*n2;
 
-        ds = s_pred - s_ref;
-        s_c2e = ds.' * n_ref;
+        c(k) =  s_c2e - track_half_width;  k = k + 1;
+        c(k) = -s_c2e - track_half_width;  k = k + 1;
 
-        c = [c;
-            s_c2e - track_half_width;
-            -s_c2e - track_half_width];
-        
-        %% save u_prev
-        u_prev = u_i;
+        u_prev(1) = un_i;
+        u_prev(2) = deltan_i;
     end
-
-    c   = double(c);
-    ceq = double(ceq);
-
 end
